ed2f8f09bedad536babff3993a6b157b4b5874e7
haobu <haobu@live.unc.edu>
2022-03-15 16:49:16 -0400

    add func in flips.js

--- flips.js
+++ flips.js
@@ -1,9 +1,10 @@
 // Import the coinFlip function from your coin.mjs file
-import { coinFlips } from "./modules/coin.mjs";
+import { coinFlips, countFlips } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 // Call the coinFlip function and put the return into STDOUT
 const args = require('minimist')(process.argv.slice(2))
 //console.log(args);
 //console.log(args.number);
-console.log(coinFlips(args.number));
+const result = coinFlips(args.number);
+console.log(result, countFlips(result));
